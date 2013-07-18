4aeab04d7cc3107ca56a274a0c0ab74aa009bf22
Kate Green <kate.green@chronicle.com>
2013-07-18 16:34:29 -0400

    Getting tests working again.

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -1,6 +1,8 @@
 'use strict';
 
 describe('Controller: MainCtrl', function () {
+  var $httpBackend,
+      elements = '// Global values\n// main variables for the site\n// --------------------------------------------------\n@primary:                   #36434c;\n@secondary:                 #edf7ff;\n@tertiary:                  #B6CBD9;\n@base-color :               @primary;\n@heading-color:             #36434d;\n@secondary-heading-color:   #0088cc;';
 
   // load the controller's module
   beforeEach(module('colorOrganizerApp'));
@@ -9,11 +11,18 @@ describe('Controller: MainCtrl', function () {
     scope;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope, $injector) {
+    $httpBackend = $injector.get('$httpBackend');
+    $httpBackend.when('GET', 'styles/colors.less').respond(elements);
+
+    $httpBackend.expectGET('styles/colors.less');
+
     scope = $rootScope.$new();
     MainCtrl = $controller('MainCtrl', {
       $scope: scope
     });
+
+    $httpBackend.flush();
   }));
 
   it('gets a valid object from Color', function() {
--- test/spec/services/Colors.js
+++ test/spec/services/Colors.js
@@ -1,23 +1,40 @@
 'use strict';
 
 describe('Service: Colors', function () {
+  var $httpBackend,
+      elements = '// Global values\n// main variables for the site\n// --------------------------------------------------\n@primary:                   #36434c;\n@secondary:                 #edf7ff;\n@tertiary:                  #B6CBD9;\n@base-color :               @primary;\n@heading-color:             #36434d;\n@secondary-heading-color:   #0088cc;';
 
   // load the service's module
   beforeEach(module('colorOrganizerApp'));
 
   // instantiate service
   var Colors;
-  beforeEach(inject(function (_Colors_) {
+  beforeEach(inject(function (_Colors_,$injector) {
     Colors = _Colors_;
+
+    $httpBackend = $injector.get('$httpBackend');
+    $httpBackend.when('GET', 'styles/colors.less').respond(elements);
   }));
 
+  afterEach(function() {
+   $httpBackend.verifyNoOutstandingExpectation();
+   $httpBackend.verifyNoOutstandingRequest();
+  });
+
   it('should return an object', function () {
-    expect(Colors.get()).toEqual(jasmine.any(Object));
+    $httpBackend.expectGET('styles/colors.less');
+    var colors = Colors.get();
+
+    $httpBackend.flush();
+
+    expect(colors).toEqual(jasmine.any(Object));
   });
 
   it('should return an object where all colors have a color (not just a link)', function() {
     var colors = Colors.get();
     var hasColor = true;
+
+    $httpBackend.flush();
 
     var testColors = function(colors) {
       angular.forEach(colors, function(group) {
