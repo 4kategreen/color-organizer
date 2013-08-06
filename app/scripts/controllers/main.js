873af9f9d387ec80d72240ebc51c79f3be7c02bc
4kategreen <kate@kategreen.us>
2013-08-06 13:51:19 -0400

    Controller update. Adding an id, though it feels like cheating. Also have a class-adding method. Refined the button active states.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,11 +3,11 @@
 angular.module('colorOrganizerApp')
   .controller('MainCtrl', ['$scope', 'Colors', function ($scope, Colors) {
     $scope.colorSets = [
-      { 'name': 'Common','file': 'colors.less' },
-      { 'name': 'CHE', 'file': 'colors-che.less' },
-      { 'name': 'COP', 'file': 'colors-cop.less' }
+      { 'id': 0, 'name': 'Common','file': 'colors.less' },
+      { 'id': 1, 'name': 'CHE', 'file': 'colors-che.less' },
+      { 'id': 2, 'name': 'COP', 'file': 'colors-cop.less' }
     ];
-    $scope.active = $scope.colorSets[0].name;
+    $scope.active = $scope.colorSets[0];
     $scope.manualStyle = '#fff';
 
     $scope.elements = Colors.get($scope.colorSets[0].file);
@@ -25,7 +25,18 @@ angular.module('colorOrganizerApp')
     // }
 
     $scope.switchView = function(set) {
-      $scope.active = $scope.colorSets[set].name;
+    	console.log('in switch');
+      $scope.active = $scope.colorSets[set];
       $scope.elements = Colors.get($scope.colorSets[set].file);
+
+      $scope.elements.then(function(elements) {
+      	$scope.elements = elements;
+      }, function(status) {
+      	console.log(status);
+      });
+    };
+    
+    $scope.setClass = function(bool) {
+    	return bool ? 'btn-primary' : '';
     }
   }]);
