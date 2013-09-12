dac6964eb4f95b1ca5e559136369be0a9ee6b5bc
Kate Green <kate.green@chronicle.com>
2013-09-12 17:19:36 -0400

    new methods to do the work more succinctly. eventually anyway.

--- app/scripts/services/Colors.js
+++ app/scripts/services/Colors.js
@@ -2,44 +2,72 @@
 
 angular.module('colorOrganizerApp')
   .service('Colors', ['$http', '$q', function ($http, $q) {
-    /**
-      Parses any variable links to get the real color.
-    */
-    var parseLinks = function(testColor, colorList) {
-      var selectedColor = false;
+    var methods = {
+      parseLines: function(lines) {
+        var variables = [];
 
-      angular.forEach(colorList, function(color) {
-        if (testColor === color[0]) {
-          selectedColor = color[1];
-        }
-      });
+        var comment = /^(\/{2})\s+(.+)/gi,
+            sectionComment = /^(\/{2})\s+(-+)/gi,
+            ele = /^(@.+):\s+(.+);\s*\/{0,2}\s*(.*)/gi;
 
-      return selectedColor || 'No Color Found';
-    };
-
-    /**
-      Takes all vars from a parsed list and makes one single list to test against.
-
-      Colors is default
-    */
-    var createVariableList = function(vars, type) {
-      var colorList = [];
-
-      angular.forEach(vars, function(group) {
-        if (type === 'size') {
-          angular.forEach(group.sizes, function(size) {
-            colorList.push([size.name, size.size]);            
-          });
-        } else {
-          angular.forEach(group.colors, function(color) {
-            if (color.color) {
-              colorList.push([color.name, color.color]);
+        angular.forEach(lines, function(line) {
+          // comments and sections
+          if (comment.exec(line)) {
+            if (sectionComment.exec(line)) {
+              variables.push({
+                'type': 'section',
+                'value': line
+              });
+            } else {
+              variables.push({
+                'type': 'comment',
+                'value': line
+              });
             }
-          });
-        }
-      });
+          }
 
-      return colorList;
+          // element
+        });
+      },
+      /**
+        Parses any variable links to get the real color.
+      */
+      parseLinks: function(testColor, colorList) {
+        var selectedColor = false;
+
+        angular.forEach(colorList, function(color) {
+          if (testColor === color[0]) {
+            selectedColor = color[1];
+          }
+        });
+
+        return selectedColor || 'No Color Found';
+      },
+
+      /**
+        Takes all vars from a parsed list and makes one single list to test against.
+
+        Colors is default
+      */
+      createVariableList: function(vars, type) {
+        var colorList = [];
+
+        angular.forEach(vars, function(group) {
+          if (type === 'size') {
+            angular.forEach(group.sizes, function(size) {
+              colorList.push([size.name, size.size]);
+            });
+          } else {
+            angular.forEach(group.colors, function(color) {
+              if (color.color) {
+                colorList.push([color.name, color.color]);
+              }
+            });
+          }
+        });
+
+        return colorList;
+      }
     };
 
     return {
@@ -127,7 +155,7 @@ angular.module('colorOrganizerApp')
                 // two elements, please. for now.
               angular.forEach(mathHolder, function(ele){
                 var mathInfo = {},
-                    sizes = createVariableList(elements, 'size'),
+                    sizes = methods.createVariableList(elements, 'size'),
                     calc = /(.+)\s+([\+\-\*\/])\s+(.+)/.exec(ele[2]);
 
                 mathInfo.name = ele[1];
@@ -154,12 +182,12 @@ angular.module('colorOrganizerApp')
           }
 
           // do linking
-          colorList = createVariableList(elements);
+          colorList = methods.createVariableList(elements);
 
           angular.forEach(elements, function(group) {
             angular.forEach(group.colors, function(colors) {
               if (colors.link) {
-                colors.color = parseLinks(colors.link, colorList);
+                colors.color = methods.parseLinks(colors.link, colorList);
               }
             });
           }); // end linking
